6c0a47def66b9086a355c6b3ffb35420010cf057
Юрий Тищенко <yuri.tishchenko@baltinfocom.ru>
2023-11-29 18:53:02 +0300

    dd

--- yatube_parsing/spiders/yatube.py
+++ yatube_parsing/spiders/yatube.py
@@ -4,7 +4,21 @@ import scrapy
 class YatubeSpider(scrapy.Spider):
     name = "yatube"
     allowed_domains = ["51.250.32.185"]
-    start_urls = ["https://51.250.32.185"]
+    start_urls = [" http://51.250.32.185/"]
 
     def parse(self, response):
-        pass
+        print('|?||??|?|?|?|?|?|?|?|?|?|??||?')
+        print(len(response.css('div.container div.card-body')))
+        print('|?||??|?|?|?|?|?|?|?|?|?|??||?')
+        for post in response.css('div.container div.card-body'):
+            text = ' '.join(
+                t.strip() for t in post.css('p::text').getall()
+            ).strip()
+            yield {
+                'author': post.css('strong::text').get(),
+                'text': text,
+                'date': post.css('small::text').get(),
+            }
+        next_page = response.xpath("//a[contains(., 'Следующая')]/@href").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
